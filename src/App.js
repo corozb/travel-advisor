ece5965d1ac284170ca1375dbd72eb35de5f25df
cristian.orozco <cristian.orozco@globant.com>
2021-08-23 18:27:12 -0500

    Travel Advisor - Web app to travel with free apis

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { Grid, CssBaseline } from '@material-ui/core'
+
+import { getWeatherData, getPlacesData } from './api'
+import Header from './components/Header/Header'
+import List from './components/List/List'
+import Map from './components/Map/Map'
 
 function App() {
+  const [type, setType] = useState('restaurants')
+  const [rating, setRating] = useState('')
+
+  const [places, setPlaces] = useState([])
+  const [weatherData, setWeatherData] = useState([])
+  const [filteredPlaces, setfilteredPlaces] = useState([])
+
+  const [autocomplete, setAutocomplete] = useState(null)
+  const [childClicked, setChildClicked] = useState(null)
+  const [coordinates, setCoordinates] = useState({})
+  const [bounds, setBounds] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoordinates({ lat: latitude, lng: longitude })
+    })
+  }, [])
+
+  useEffect(() => {
+    const filtered = places.filter((place) => place.rating > rating)
+
+    setfilteredPlaces(filtered)
+  }, [rating]) // eslint-disable-line
+
+  useEffect(() => {
+    if (bounds.ne && bounds.sw) {
+      setIsLoading(true)
+
+      getWeatherData(coordinates.lat, coordinates.lng).then((data) => setWeatherData(data))
+
+      getPlacesData(type, bounds?.ne, bounds?.sw).then((data) => {
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0))
+        setfilteredPlaces([])
+        setIsLoading(false)
+      })
+    }
+  }, [bounds, type])
+
+  const onLoad = (autoC) => setAutocomplete(autoC)
+
+  const onPlaceChanged = () => {
+    const lat = autocomplete.getPlace().geometry.location.lat()
+    const lng = autocomplete.getPlace().geometry.location.lng()
+    setCoordinates({ lat, lng })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <CssBaseline />
+      <Header onLoad={onLoad} onPlaceChanged={onPlaceChanged} />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List
+            childClicked={childClicked}
+            isLoading={isLoading}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            rating={rating}
+            setRating={setRating}
+            setType={setType}
+            type={type}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            coordinates={coordinates}
+            places={filteredPlaces.length ? filteredPlaces : places}
+            setBounds={setBounds}
+            setChildClicked={setChildClicked}
+            setCoordinates={setCoordinates}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
-export default App;
+export default App
